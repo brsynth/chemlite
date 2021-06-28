b45cc5ae3eefb48db3bd4ecbea0afc80769b08e2
breakthewall <joan.herisson@univ-evry.fr>
2021-06-28 13:57:44 +0200

    doc: mv rst and conf.py files into source folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 #sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 # -- Project information -----------------------------------------------------
@@ -41,11 +41,14 @@ master_doc = 'index'
 
 # source_suffix = ['.rst', '.md']
 
-# autodoc_default_flags = [
-#     'members',
-#     'inherited-members',
-#     'show-inheritance'
-# ]
+# -- Sphinx AutoAPI ----------------------------------------------------------
+autoapi_type = "python"
+autoapi_dirs = ["../../chemlite"]
+autoapi_options = [
+    "members",
+    "undoc-members",
+    "show-inheritance",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -61,7 +64,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
