11d454d2a05b0a4422fbeab1732c5ed135add48d
Joan Hérisson <joan.herisson@univ-evry.fr>
2021-10-13 12:23:06 +0200

    trigger

--- chemlite/__init__.py
+++ chemlite/__init__.py
@@ -4,5 +4,4 @@ from chemlite.Compound import Compound
 from chemlite.Object import Object
 from chemlite._version import __version__
 
-
 __all__ = ['Pathway', 'Reaction', 'Compound', 'Object', __version__]
